50831ee0c804aeee0470995682c1df263a8c1ac0
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-09-28 12:40:16 +0000

    Create the first component FunctionalGreeting

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,11 @@
+// This line is not needed in newer versions of React
+// But it makes our code also backwards compatible with previous releases.
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>
+
+export default FunctionalGreeting
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,8 @@
+// This line is not needed in newer versions of React
+// But it makes our code also backwards compatible with previous releases.
+import React from "react";
+
+
+const FunctionalGreetingWithProps = () => <h1>Hello from React y mi pene!</h1>
+
+export default FunctionalGreetingWithProps

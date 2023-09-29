929bc4de6913f1f3fb5d9a7e55bcc596615d8de1
Paulo Arbeláez <pauloarbelaez@gmail.com>
2023-09-29 09:30:36 +0000

    Recommit for rights fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// Each component needs to be imported
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
